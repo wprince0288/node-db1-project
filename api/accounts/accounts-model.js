f56a5a5e8d65b65ffb9a0e536d1a9c01c0eec388
Walter Prince <wprince0288@gmail.com>
2025-02-08 22:19:36 -0600

    wippie

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -13,8 +13,9 @@ const create = async account => {
   return getById(id)
 }
 
-const updateById = (id, account) => {
-
+const updateById = async (id, account) => {
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
 const deleteById = id => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -26,7 +26,10 @@ router.post(
   md.checkAccountNameUnique,
   async (req, res, next) => {
     try {
-      const newAccount = await Account.create(req.body)
+      const newAccount = await Account.create({
+        name: req.body.name.trim(),
+        budget: req.body.budget,
+      })
       res.status(201).json(newAccount)
     } catch (err) {
       next(err);
@@ -37,9 +40,10 @@ router.put(
   '/:id',
   md.checkAccountId,
   md.checkAccountPayload,
-  md.checkAccountNameUnique,
-  (req, res, next) => {
+  async (req, res, next) => {
+    const updated = await Account.updateById(req.params.id, req.body)
     try {
+      res.json(updated)
       res.json('update account')
     } catch (err) {
       next(err);
